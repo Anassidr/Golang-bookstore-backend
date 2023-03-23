5f953fbd26425e2e50f88e0a66f0e24fdc884d03
Anassidr <kamalanass96@gmail.com>
2023-03-23 18:41:35 +0100

    CreateBook

--- pkg/controllers/book-controllers.go
+++ pkg/controllers/book-controllers.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/anassidr/go-bookstore/pkg/models"
+	"github.com/anassidr/go-bookstore/pkg/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -15,7 +16,7 @@ var NewBook models.Book
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
 	res, _ := json.Marshal(newBooks)
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
@@ -28,4 +29,19 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("error while parsing")
 	}
 	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook) //extract data from the request body and store it in the CreateBook object
+	b := CreateBook.CreateBook()   //The CreateBook object's method "CreateBook" is called to persist the book data in the database
+	res, _ := json.Marshal(b)      // book object is serialized
+	w.WriteHeader(http.StatusOK)   // http response is written
+	w.Write(res)                   // response body will contain a JSON of the created book
+}
+
+func DeleteBook(w h)
